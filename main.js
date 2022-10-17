dea319e55bddcfa86acdd162bcb06336ac7228ff
MaciusGaming <112561728+MaciusGaming@users.noreply.github.com>
2022-10-17 18:35:59 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function Michal() {
+    var michalbox = document.getElementById("MichalBox");
+    michalbox.style.display = "block";
+}
+
+function Kuba() {
+    var michal = document.getElementById("Kuba");
+    michal.style.display = "none";
+}
+
+function Jaca() {
+    var michal = document.getElementById("Jaca");
+    michal.style.display = "none";
+}
+
+function Damianek() {
+    var michal = document.getElementById("Damianek");
+    michal.style.display = "none";
+}
+
+function Stasio() {
+    var michal = document.getElementById("Stasio");
+    michal.style.display = "none";
+}
